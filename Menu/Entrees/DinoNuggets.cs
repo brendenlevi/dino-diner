d17c4788ae4495ac2963535f9632c6ff2c65d95d
Brenden Levi <brendenlevi@users.campus>
2019-09-09 17:18:07 -0500

    9-9 in class

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>(){"Chicken Nugget",
+                    "Chicken Nugget", "Chicken Nugget",
+                    "Chicken Nugget", "Chicken Nugget",
+                    "Chicken Nugget" };
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 59 * 6;
+        }
+
+        public void AddNugget()
+        {
+            this.Price += .25;
+            this.Calories += 59;
+            this.Ingredients.Add("Chicken Nugget");
+        }
+    }
+}
