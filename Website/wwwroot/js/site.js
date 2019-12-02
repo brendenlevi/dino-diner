34262bfc02f739cfa0fbc59ce2f84d528486d184
brendenlevi <levib1399@gmail.com>
2019-12-01 20:35:12 -0600

    Working on Milestone 2

--- Website/wwwroot/js/site.js
+++ Website/wwwroot/js/site.js
@@ -2,31 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-
-function hideAllMenu() {
-    var elements = document.getElementsByClassName("menu-div");
-
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].style.display = "none";
-    }
-}
-
-function onClickCombos() {
-    hideAllMenu();
-    document.getElementById("CombosDiv").style.display = "block";
-}
-
-function onClickEntrees() {
-    hideAllMenu();
-    document.getElementById("EntreesDiv").style.display = "block";
-}
-
-function onClickSides() {
-    hideAllMenu();
-    document.getElementById("SidesDiv").style.display = "block";
-}
-
-function onClickDrinks() {
-    hideAllMenu();
-    document.getElementById("DrinksDiv").style.display = "block";
-}
